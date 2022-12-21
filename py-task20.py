3febbced46e0623163024b8dc670c6903238fc73
Alexandr <jwchaganav@gmail.com>
2022-12-21 11:53:22 +0400

    создал словарь engl через цикл

--- py-task20.py
+++ py-task20.py
@@ -1,3 +1,5 @@
+import os
+os.system('cls')
 # create to dictonary
 dictEn = {}
 dictRu = {}
@@ -12,6 +14,7 @@ F = ["F", "H", "V", "W", "Y"] # 4 очка
 K = ["K",] # 5 очков
 J = ["J", "X"] # 8 очков
 Q = ["Q", "Z"] # 10 очков
+listEn = [[A,1],[D,2],[B,3],[F,4],[K,5],[J,8],[Q,10]]
 # russia
 ruA = ["А", "В", "Е", "И", "Н", "О", "Р", "С", "Т"] # 1 очка
 ruD = ["Д", "К", "Л", "М", "П", "У"] # 2 очка
@@ -20,26 +23,12 @@ ruI = ["Й", "Ы"] # 4 очка
 ruJ = ["Ж", "З", "Х", "Ц", "Ч"] # 5 очков
 ruSh = ["Ш", "Э", "Ю"] # 8 очков
 ruF = ["Ф", "Щ", "Ъ"] # 10 очков
-dRu = [[ruA,1],[ruD,2],[ruB,3],[ruI,4],[ruJ,5],[ruSh,8],[ruF,10]]
-Fill(dictEn,A,1)
-Fill(dictEn,D,2)
-Fill(dictEn,B,3)
-Fill(dictEn,F,4)
-Fill(dictEn,K,5)
-Fill(dictEn,J,8)
-Fill(dictEn,Q,10)
-# print(dictEn)
+listRu = [[ruA,1],[ruD,2],[ruB,3],[ruI,4],[ruJ,5],[ruSh,8],[ruF,10]]
+for l in listEn:
+    Fill(dictEn, l[0], l[1])
+print(dictEn)
 #
-for r in dRu:
-    Fill(dictRu,r[0],r[1])
-    # print(r[0])
-    # for item in r:
-    #     print(item[0][0])
-# Fill(dictRu,ruA,1)
-# Fill(dictRu,ruD,2)
-# Fill(dictRu,ruB,3)
-# Fill(dictRu,ruI,4)
-# Fill(dictRu,ruJ,5)
-# Fill(dictRu,ruSh,8)
-# Fill(dictRu,ruF,10)
+for l in listRu:
+    Fill(dictRu,l[0],l[1])
+
 print(dictRu)
